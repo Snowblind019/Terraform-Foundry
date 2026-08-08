aa6504ce9efbd8ad3a92ab8eae800345569910f8
Snowblind019 <snowblind019@proton.me>
2026-08-08 15:55:07 -0700

    Added comments

--- Deploying Infrastructure with Terraform/01 Launching an EC2 instance/main.tf
+++ Deploying Infrastructure with Terraform/01 Launching an EC2 instance/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "WinterdayEC2" {
     }
 }
 
-#this is how you add multiple resources, you have to list each individually 
+#this is how you add multiple resources, you have to list each individually, but the name after the "aws_instance" has to be unique from the other ones
 resource "aws_instance" "SnowydayEC2" {
     ami = "ami-091124c3965bce679"
     instance_type = "t3.micro"
@@ -20,3 +20,5 @@ resource "aws_instance" "SnowydayEC2" {
         Name = "SnowydayEC2"
     }
 }
+
+#you're only able to use commands related to the specific provider; if provider is azurerm, aws commands like aws_instance wouldn't work. Also the core of Terraform is the same regardless of provider, for the provider specific commands the docs are there for easy reference.
