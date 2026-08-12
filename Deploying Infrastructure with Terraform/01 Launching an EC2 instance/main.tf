8e04024c759e1f76606210705c1818d1b7386c7d
Snowblind019 <snowblind019@proton.me>
2026-08-12 15:14:22 -0700

    Removing resources

--- Deploying Infrastructure with Terraform/01 Launching an EC2 instance/main.tf
+++ Deploying Infrastructure with Terraform/01 Launching an EC2 instance/main.tf
@@ -34,3 +34,10 @@ terraform {
     }
   }
 }
+
+#to delete all these resources there are 2 ways to do it.
+#'terraform destroy' will delete everything in the folder, but if you want to specify specific things, the '-target' flag should be used. To use the -target flag you'll need to specify the resource name and the local name of the specified file.
+
+#for example, I have the WinterdayEC2 and SnowydayEC2, I want to delete the latter one and thus would need to specify the resource "aws_instance.SnowydayEC2" in the command. The command would look like "terraform destroy -target aws_instance.SnowydayEC2" The name is case sensitive and so has to match exactly as written in the code.
+
+#you can also make a resource not be added by commenting it out by doing /* text */
