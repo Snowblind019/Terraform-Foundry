a79f9c71719d5c96daab2300af06723d068563ba
Snowblind019 <snowblind019@proton.me>
2026-08-12 10:15:27 -0700

    Specifying providers

--- Deploying Infrastructure with Terraform/01 Launching an EC2 instance/main.tf
+++ Deploying Infrastructure with Terraform/01 Launching an EC2 instance/main.tf
@@ -22,3 +22,15 @@ resource "aws_instance" "SnowydayEC2" {
 }
 
 #you're only able to use commands related to the specific provider; if provider is azurerm, aws commands like aws_instance wouldn't work. Also the core of Terraform is the same regardless of provider, for the provider specific commands the docs are there for easy reference.
+
+#by default when specifying a provider, it searches the hashicorp provider registry. So when specifying a provider like digitalocean who isn't a hashicorp maintained provider it won't find it. All non-hasshicorp maintained providers have to be specified like provider "greg-oc/87347" {} or provider "digitalocean/digitalocean" {}.
+
+#if you're getting issues running the 'terraform init' command when specifying one of these providers, you can search them up and get the specified template from Hashicopr themselves. This also works for hashicorp maintained ones like aws, but for ease of access those can be simplified to just provider "aws" {}.
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
